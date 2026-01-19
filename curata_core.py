30d6b93285390c1febe9e4fca4b091bb787215e4
AftabAliRavji <41339774+AftabAliRavji@users.noreply.github.com>
2026-01-19 19:24:57 +0000

    Update curata_core.py
    commenting out secrets print out

--- curata_core.py
+++ curata_core.py
@@ -385,8 +385,8 @@ def logout_button():
 # ============================================================
 
 def main_app():
-    st.write("Secrets keys:", list(st.secrets.keys()))
-    st.write("Raw secrets:", st.secrets)
+    # st.write("Secrets keys:", list(st.secrets.keys()))
+    # st.write("Raw secrets:", st.secrets)
     init_default_state()
 
     # Auto-restore session only AFTER login, from Supabase
@@ -1249,5 +1249,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
